3663ca292396e52e4bb4e74b3e8c8933fb2d069e
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2023-11-07 21:01:33 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,9 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-git clone https://github.com/fw876/helloworld/tree/main/luci-app-ssr-plus
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.100.1"            # 修改openwrt后台地址(填0为关闭)
